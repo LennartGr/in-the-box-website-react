a586974e950962901144bf4904b8ce13c9b62d4a
Lennart Grosskreutz <lennart.grosskreutz@student.kit.edu>
2023-01-18 13:35:12 +0100

    Render button to change language
    Not yet connected to content

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import './App.css'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
+import { LanguageProvider } from './containers/Language';
+import LanguageSelector from './components/LanguageSelector';
+
 export default function App() {
   return (
-    <div id="app">
-      <Hero />
-      <Navbar />
-    </div>
-    
+    <LanguageProvider>  
+      <div id="app">
+        <LanguageSelector />
+        <Hero />
+        <Navbar />
+      </div>
+    </LanguageProvider>
   )
 }
 
