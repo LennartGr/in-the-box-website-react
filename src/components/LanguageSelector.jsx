4125fc37273c4dbc0d5dbee58de1d121424a5520
Lennart Grosskreutz <lennart.grosskreutz@student.kit.edu>
2023-01-27 00:54:44 +0100

    Improve language selector
    No HTML select anymore, but custom one

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,5 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './css/LanguageSelector.css'
+
+import flags from "../images/flags/flags"
 
 import { languageOptions } from '../languages';
 import { LanguageContext } from '../containers/Language';
@@ -7,17 +9,43 @@ import { LanguageContext } from '../containers/Language';
 export default function LanguageSelector() {
   const { userLanguage, userLanguageChange } = useContext(LanguageContext);
 
-  // set selected language by calling context method
-  const handleLanguageChange = e => userLanguageChange(e.target.value);
+  //state that manages whether the language bar is displayed
+  const [optionsShown, setOptionsShown] = useState(false)
+
+  //returns full string of current language
+  function getCurrentLanguage() {
+    return languageOptions[userLanguage]
+  }
+
+  //retrieves the dropdown-menu / shows it
+  function toggleShowOptions() {
+    setOptionsShown(val => !val)
+  }
+
+  //create one option for each language
+  const options = Object.entries(languageOptions).map(([id, name]) => (
+    <div
+      key={id}
+      className="option"
+      onClick={function () {
+        userLanguageChange(id)
+        toggleShowOptions()
+      }}>
+      <img className="flag" src={flags[id]} />
+      {name}   
+    </div>
+  ))
 
   return (
-    <select id="language-selector"
-      onChange={handleLanguageChange}
-      value={userLanguage}
-    >
-      {Object.entries(languageOptions).map(([id, name]) => (
-        <option key={id} value={id}>{name}</option>
-      ))}
-    </select>
-  );
+    <div className="select-box">
+      {/*conditionally add active to className of options-container*/}
+      <div className={`options-container${optionsShown ? " active" : ""}`}>
+        {options}
+      </div>
+      <div className="selected" onClick={toggleShowOptions}>
+        <img className="flag" src={flags[userLanguage]} />
+        {getCurrentLanguage()}
+      </div>
+    </div>
+  )
 };
---
+++ src/images/flags/flags.jsx
@@ -0,0 +1,7 @@
+import enFlag from "./en.png"
+import frFlag from "./fr.png"
+
+//TODO make this be done automatically for every new language
+export default {en: enFlag, fr: frFlag}
+
+
