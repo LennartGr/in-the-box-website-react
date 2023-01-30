eacac759b7347e95d04e126e1e8491b3e0dd7540
Lennart Grosskreutz <lennart.grosskreutz@student.kit.edu>
2023-01-30 11:13:34 +0100

    Finish redesign: Add proper colors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,10 @@ import logo from '../images/logo.svg'
 export default function Hero() {
     return (
         <div id="hero">
-            <img id="logo" src={logo} alt="our logo" />
+            <div id="header-images">
+                <img id="logo-background-blur" src={logo} />
+                <img id="logo" src={logo} alt="our logo" />
+            </div>
             <h1 id="title">In The Box</h1>
         </div>
     )
