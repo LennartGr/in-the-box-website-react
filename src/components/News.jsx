92d21fbc0af2044c5f560aa7ffc13a0ba948d024
Lennart Grosskreutz <lennart.grosskreutz@student.kit.edu>
2023-02-28 11:46:37 +0100

    News first version

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,6 +4,7 @@ export default function News() {
     return (
         <div id="section--news" className="section">
             <h1><Text tid="news" /></h1>
+            <p><Text tid="news-no-news" /></p>
         </div>
     )
 }
