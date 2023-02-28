60cdccd2b5cf45894a854cc28480fe0b41cda9f4
Lennart Grosskreutz <lennart.grosskreutz@student.kit.edu>
2023-02-28 11:04:13 +0100

    Contact first version

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,12 @@
 import { Text, LanguageContext } from '../containers/Language';
 
 export default function Contact() {
+    // idea: button that copies mail to clipboard
     return (
         <div id="section--contact" className="section">
             <h1><Text tid="contact" /></h1>
+            <p><Text tid="contact-mail" /></p>
+            <p><a href="in.the.box.klgb@gmail.com">in.the.box.klgb@gmail.com</a></p>
         </div>
     )
 }
