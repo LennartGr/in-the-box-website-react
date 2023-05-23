dcf29e8a3d64601f6372c222c724a8a9b9ae4f5d
Lennart Grosskreutz <lennart.grosskreutz@student.kit.edu>
2023-05-23 13:38:48 +0200

    Update home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,9 @@ export default function Home() {
     return (
         <div id="section--home" className="section">
             <h1><Text tid="home" /></h1>
-            <h2><Text tid="home-objective-title" /></h2>
-            <p><Text tid="home-objective-details" /></p>
+            {/* <h2><Text tid="home-objective-title" /></h2> */}
+            <p><Text tid="home-first-paragraph" /></p>
+            <p><Text tid="home-second-paragraph" /></p>
         </div>
     )
 }
